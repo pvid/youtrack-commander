7e1bb18ad5b63591fd37c6156171991fd21beec0
Pavol Vidlicka <pavol.vidlicka@firma.seznam.cz>
2020-08-31 15:31:08 +0200

    feat: list issue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.0] - 2020-08-31
+### Added
+- List issues
+
 ## [0.1.0] - 2020-08-29
 ### Added
 - Execute a command on a Youtrack issue
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use anyhow::{anyhow, Result};
 use client::YoutrackService;
 pub use conf::Configuration;
 pub use opts::Opts;
+use term_grid::{Cell, Direction, Filling, Grid, GridOptions};
 
 #[derive(Debug)]
 pub struct Command {
@@ -17,10 +18,23 @@ pub struct Command {
 type ParsedCommand = String;
 
 pub trait CommandService {
-    fn execute_command<'a>(&self, command: Command) -> Result<Vec<ParsedCommand>>;
+    fn execute_command(&self, command: Command) -> Result<Vec<ParsedCommand>>;
+}
+
+#[derive(Debug)]
+pub struct ListedIssue {
+    pub id: String,
+    pub summary: String,
+}
+
+pub trait ListingService {
+    fn list_issues(&self, query: String, limit: Option<u8>, offset: u8)
+        -> Result<Vec<ListedIssue>>;
 }
 
 pub fn run(opts: Opts, conf: Configuration) -> Result<()> {
+    let youtrack_service = YoutrackService::new(&conf.youtrack_url, &conf.auth_token);
+
     match opts {
         Opts::Issue {
             issue_id,
@@ -32,13 +46,31 @@ pub fn run(opts: Opts, conf: Configuration) -> Result<()> {
                 query: command,
                 comment,
             };
-            let command_service = YoutrackService::new(&conf.youtrack_url, &conf.auth_token);
 
-            let executed_commands = command_service.execute_command(command)?;
+            let executed_commands = youtrack_service.execute_command(command)?;
             println!("Executed commands:");
             executed_commands
                 .iter()
                 .for_each(|command| println!("    {}", command));
+            Ok(())
+        }
+        Opts::List {
+            query,
+            limit,
+            offset,
+        } => {
+            let issues = youtrack_service.list_issues(query, limit, offset.unwrap_or(0))?;
+            let mut grid = Grid::new(GridOptions {
+                filling: Filling::Spaces(2),
+                direction: Direction::LeftToRight,
+            });
+
+            for issue in issues {
+                grid.add(Cell::from(issue.id));
+                grid.add(Cell::from(issue.summary));
+            }
+
+            print!("{}", grid.fit_into_columns(2));
             Ok(())
         }
         Opts::Console => Err(anyhow!("Console mode not implemented")),
